68ca4f5d831f2c6ad15c54d9e2c98e1c539e8822
Nouran <nouransabry39@gmail.com>
2022-10-28 20:26:57 +0200

    Added check to make sure instructor exists before adding a course

--- Controllers/instructor-controller.js
+++ Controllers/instructor-controller.js
@@ -1,6 +1,6 @@
+const mongoose = require('mongoose');
 const instructorModel = require("../Models/Instructor");
 const courseModel = require("../Models/Course");
-
 
 //View all instructors
 const getAllInstructors = async (req, res) => {
@@ -23,34 +23,48 @@ const addCourse = async(req , res) => {
    
     //Uncomment this bit to delete a specific course
 
-    // courseModel.deleteOne({ NameOfCourse: 'TestCourseName' }, function (err) {
+    // try{
+    
+    // courseModel.deleteMany({ NameOfCourse: 'PleaseWork' }, function (err) {
     //     if(err) console.log(err);
     //     console.log("Successful deletion");
     //   });
 
+    const Instructor = req.params.id;
+   
+   //check if the Instructor exists first (this check will probably be removed when authentication is implemented)
+   const result = await instructorModel.findOne({_id:mongoose.Types.ObjectId(Instructor)});
+   if(result !== null){
+        try{
+    
+            // get the details from the body of the request
+            const{NameOfCourse,
+                CourseSubtitle,
+                Summary,
+                Subject,
+                LevelOfCourse,
+                Cost} = req.body;
+            
+            const course = await courseModel.create(
+                {NameOfCourse,
+                CourseSubtitle,
+                Instructor,
+                LevelOfCourse,
+                Summary,
+                Subject,
+                Cost});
+        
+            res.status(200).json(result);
+            
+        
+        }catch(error){
+            res.status(400).json({error:error.message})
+        }
 
-    // get the details from the body of the request
-    const{NameOfCourse,
-        InstructorId,
-        CourseSubtitle,
-        Summary,
-        Subject,
-        LevelOfCourse,
-        Cost} = req.body;
-    try{
-        const course = await courseModel.create(
-            {NameOfCourse,
-            InstructorId,
-            CourseSubtitle,
-            Summary,
-            Subject,
-            LevelOfCourse,
-            Cost});
-       
-        res.status(200).json(course);
-    }catch(error){
-        res.status(400).json({error:error.message})
-    }
+   } else{
+    res.status(400).json({error:"Please enter a valid Instructor Id"});
+}
+    
 }
 
 
