0b2395e25cd0315ada65423fb7adc9243cafe31c
Maryam Sherif <mariamsherif2.ms@gmail.com>
2022-11-28 17:47:31 +0200

    subtitle routes

--- backend/Routes/Subtitle-route.js
+++ backend/Routes/Subtitle-route.js
@@ -2,5 +2,8 @@ const express= require("express");
 
 const router = express.Router();
 
+const {getAllSubtitles} = require('../Controller/subtitle-controller');
+
+router.get('/getAllSubtitles',getAllSubtitles);
 
 module.exports=router;
